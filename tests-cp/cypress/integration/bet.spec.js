a7bd72ed37c0e925196550226fd7d67797831e7f
Herbert Nordson <herbert_nordson@hotmail.com>
2021-12-14 11:45:25 -0300

    feat: testes finalizados!

--- tests-cp/cypress/integration/bet.spec.js
+++ tests-cp/cypress/integration/bet.spec.js
@@ -5,12 +5,29 @@ describe("bet", () => {
     cy.visit("http://localhost:3000/");
 
     cy.get('[type="button"]').click();
+
+    cy.get('[type="name"]').type("HN");
+    cy.get('[type="email"]').type("herbert");
+    cy.get('[type="password"]').type("123456");
+
+    cy.get("form > button").click();
+
+    cy.wait(500);
+
+    cy.get('[type="name"]').clear();
+    cy.get('[type="email"]').clear();
+    cy.get('[type="password"]').clear();
+
+    cy.wait(500);
+
     cy.get('[type="name"]').type("HNS");
     cy.get('[type="email"]').type("herbert@teste.com");
     cy.get('[type="password"]').type("12345678");
 
     cy.get("form > button").click();
     cy.get(".Toastify__close-button > svg").click();
+
+    cy.wait(500);
   });
 
   it("login", () => {
